9014825f9c8aae21934685a8d85d5a95f23ec30f
geunyang <gaeballza@gmail.com>
2022-03-22 09:19:32 +0900

    hello

---
+++ HelloKorea.java
@@ -0,0 +1,13 @@
+/*
+this is multiline comment
+*/
+// single line comment
+
+
+public class HelloKorea {
+	// comment
+	public static void main(String[] args) {
+		System.out.println("Hello Korea");
+
+	}
+}
