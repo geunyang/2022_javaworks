77af65cc66d440a8753b307c4defb9dc00b9004b
geunyang <gaeballza@gmail.com>
2022-05-09 16:45:38 +0900

    220509 오전

---
+++ Java_60_App_02/src/com/callor/app/service/ScoreService.java
@@ -0,0 +1,15 @@
+package com.callor.app.service;
+
+/*
+ * 클래스의 한 종류이지만 모든 method의 구현체가 없는 상태
+ * method의 이름만 정의된 상태
+ * method의 prototype 만 정의된 상태
+ * method 이름, 매개변수종류, 
+ */
+public interface ScoreService {
+	
+	// method가 이름만 있는 상태
+	public void inputScore();	
+	public void saveScore();
+
+}
